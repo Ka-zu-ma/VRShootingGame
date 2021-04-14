02ec187b9906dc4c587c08975797221b5420e0e6
Kazuma_M <km.km.kzkz00@gmail.com>
2021-04-14 18:33:10 +0900

    当たり判定処理追加、あらかじめ薬莢が生成されてしまってる問題修正

--- Assets/Scripts/HitController.cs
+++ Assets/Scripts/HitController.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HitController : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
+    //// Start is called before the first frame update
+    //void Start() {
 
-    }
+    //}
 
-    // Update is called once per frame
-    void Update() {
+    //// Update is called once per frame
+    //void Update() {
 
-    }
+    //}
 
     //当たり判定メソッド
     private void OnCollisionEnter(Collision collision) {
+        Debug.Log("OnCollisionEnter入った");
         //衝突したオブジェクトがBulletのとき
         if (collision.gameObject.CompareTag("Bullet")) {
-            Debug.Log("的と弾が衝突しました");
+            Debug.Log("Targetと弾が衝突しました");
+            //Targetを削除
+            Destroy(gameObject);
+            //弾を削除
+            Destroy(collision.gameObject);
         }
     }
 }
