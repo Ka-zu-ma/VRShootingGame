61d062a571c2be3f5a8e31b806d4229e157b7dd5
Kazuma_M <km.km.kzkz00@gmail.com>
2021-04-15 17:07:30 +0900

    いらないコード削除

--- Assets/Scripts/HitController.cs
+++ Assets/Scripts/HitController.cs
@@ -3,16 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HitController : MonoBehaviour {
-    //// Start is called before the first frame update
-    //void Start() {
-
-    //}
-
-    //// Update is called once per frame
-    //void Update() {
-
-    //}
-
     //当たり判定メソッド
     private void OnCollisionEnter(Collision collision) {
         Debug.Log("OnCollisionEnter入った");
