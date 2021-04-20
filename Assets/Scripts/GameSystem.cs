e0c27b104cab345ebaa33276a1571557c1c11d54
Kazuma_M <km.km.kzkz00@gmail.com>
2021-04-20 21:12:09 +0900

    スタートボタンを押した時にシーンを遷移させるスクリプト追加、ボタンの文字変更

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour {
+    //　スタートボタンを押したら実行する
+    public void StartGame() {
+        SceneManager.LoadScene("vrshooting");
+    }
+}
